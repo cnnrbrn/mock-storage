97cf80ba9177720665107c7c37c84b5afc8dfef6
Connor O'Brien <connorbobrien@gmail.com>
2023-08-15 22:09:56 -0300

    Update test

--- js/storage/saveToken.test.js
+++ js/storage/saveToken.test.js
@@ -5,6 +5,7 @@ const token = "12345";
 
 describe("saveToken", () => {
 	it("saves a token in storage", () => {
+		expect(localStorage.getItem(key)).toBeNull();
 		saveToken(token);
 		expect(localStorage.getItem(key)).toEqual(token);
 	});
